faec3ffe7052189cbdab8d0775294966c1cb479e
matheuscola15 <matheuscola295@gmail.com>
2023-05-04 08:54:31 -0300

    creation of filters and querys
    class 04/05

--- src/main/java/br/com/etec/matheus/cursoapi/repository/CursoRepository.java
+++ src/main/java/br/com/etec/matheus/cursoapi/repository/CursoRepository.java
@@ -1,9 +1,10 @@
 package br.com.etec.matheus.cursoapi.repository;
 
 import br.com.etec.matheus.cursoapi.model.Curso;
+import br.com.etec.matheus.cursoapi.repository.curso.CursoRepositoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CursoRepository extends JpaRepository <Curso, Integer> {
+public interface CursoRepository extends JpaRepository <Curso, Integer>, CursoRepositoryQuery {
 }
---
+++ src/main/java/br/com/etec/matheus/cursoapi/repository/curso/CursoRepositoryQuery.java
@@ -0,0 +1,14 @@
+package br.com.etec.matheus.cursoapi.repository.curso;
+
+import br.com.etec.matheus.cursoapi.model.Curso;
+import br.com.etec.matheus.cursoapi.repository.filter.CursoFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CursoRepositoryQuery {
+
+    public Page<Curso> Filtrar(CursoFilter cursoFilter, Pageable pageable);
+
+
+
+}
