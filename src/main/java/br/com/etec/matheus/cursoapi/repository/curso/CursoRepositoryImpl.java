8762805e6ee09e82c61c92752d7e96c628e67b38
matheuscola15 <matheuscola295@gmail.com>
2023-05-18 08:58:01 -0300

    typedQuery

--- src/main/java/br/com/etec/matheus/cursoapi/repository/curso/CursoRepositoryImpl.java
+++ src/main/java/br/com/etec/matheus/cursoapi/repository/curso/CursoRepositoryImpl.java
@@ -1,14 +1,16 @@
 package br.com.etec.matheus.cursoapi.repository.curso;
 
-import antlr.StringUtils;
 import br.com.etec.matheus.cursoapi.model.Curso;
 import br.com.etec.matheus.cursoapi.repository.filter.CursoFilter;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.reflect.Typed;
 import org.hibernate.Criteria;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
@@ -30,6 +32,10 @@ public class CursoRepositoryImpl implements CursoRepositoryQuery{
 
         Predicate[] predicates = criarRestricoes(cursoFilter, builder, root);
         criteria.where(predicates);
+        criteria.orderBy(builder.asc(root.get("nomecurso")));
+
+        TypedQuery<Curso> query = manager.createQuery(criteria);
+
 
         return null;
     }
@@ -37,6 +43,12 @@ public class CursoRepositoryImpl implements CursoRepositoryQuery{
     private Predicate[] criarRestricoes(CursoFilter cursoFilter, CriteriaBuilder builder, Root<Curso> root){
         List<Predicate> predicates = new ArrayList<>();
 
+        if(!StringUtils.isEmpty(cursoFilter.getNomecurso())){
+            predicates.add(builder.like(builder.lower(root.get("nomecurso")),
+                    "%" + cursoFilter.getNomecurso().toLowerCase() + "%"
+            ));
+        }
 
+        return predicates.toArray(new Predicate[predicates.size()]);
     }
 }
